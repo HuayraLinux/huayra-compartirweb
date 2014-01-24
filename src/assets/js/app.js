1a127c3f535424d09d55ddaf924b0b0df81acfa5
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-01-24 16:12:34 -0300

    Mejorando la visualización archivos (mostrando tamaño y simplificando la vista).

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -42,6 +42,15 @@ app.config(['$routeProvider', function($routeProvider) { $routeProvider.
 }]);
 
 
+app.filter('bytes', function() {
+	return function(bytes, precision) {
+		if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+		if (typeof precision === 'undefined') precision = 1;
+		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+			number = Math.floor(Math.log(bytes) / Math.log(1024));
+		return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+	}
+});
 
 app.controller("MainCtrl", function($scope) {
 	$scope.nombre = "mi nombre";
