0c8d5ce5506040f36a18609602d5afbb4655f977
Ignacio Benedetti <tranceway@gmail.com>
2014-02-07 15:11:47 -0300

    Manejo de errores al perder la conexión mientras se descarga un archivo. Manejo de errores al no tener respuesta del servidor al pedir un archivo.

--- src/assets/js/controllers/archivosCtrl.js
+++ src/assets/js/controllers/archivosCtrl.js
@@ -8,15 +8,15 @@ app.controller("ArchivosCtrl", function($scope, $http, $routeParams, $location, 
 	$scope.filtro = '';
 	$scope.mi_ip = $scope.$parent.mi_ip;
 	$scope.es_vista_mis_archivos = true;
-	
+
 	var path = "";
-	
+
 	var ruta_descargas = process.env.HOME + '/Descargas/';
-	
+
 	// Genera el directorio descargas si no existe.
 	if (! fs.existsSync(ruta_descargas))
 		fs.mkdir(ruta_descargas);
-	
+
 	if ($routeParams.url === undefined) {
 		var base_path = $scope.$parent.base;
 		var relative_path = "obtener/";
@@ -26,73 +26,79 @@ app.controller("ArchivosCtrl", function($scope, $http, $routeParams, $location, 
 		var relative_path = $routeParams.url;
 		var path = 'http://' +  relative_path;
 	}
-		
+
 	function actualizar_listado() {
 		if ($routeParams.url) {
 			var host_a_visitar = $routeParams.url.split(':')[0];
 			console.log(host_a_visitar);
 			$scope.es_vista_mis_archivos = (host_a_visitar === $scope.mi_ip);
 		}
-		
+
     $scope.en_curso = true;
-      
+
 		$http.get(path).success(function(data) {
     	$scope.en_curso = false;
 			$scope.archivos = data.archivos;
 		});
 	}
-	
+
 	$scope.descargar = function(archivo) {
 		var objeto_descarga = archivo;
 		objeto_descarga.transmitido = 0;
 		$scope.Descargas.push(objeto_descarga);
 		$scope.$parent.descargas_sin_ver += 1;
-		
+
 		archivo.bajando = true;
 
 		var file = fs.createWriteStream(ruta_descargas + archivo.name);
 
 		http.get(archivo.url, function(res) {
-			
+
 			res.on('data', function(chunk) {
 				file.write(chunk);
 				objeto_descarga.transmitido += chunk.length;
 			});
-			
+
 			res.on('end', function() {
 				objeto_descarga.transmitido = objeto_descarga.size;
 				objeto_descarga.bajando = false;
+                objeto_descarga.estado = 'success';
 				//file.close(); // produce un bug porque cierra el archivo mientras que está copiando. El archivo queda truncado.
 			});
-			
-			res.on('error', function() {
-				console.log("Error!!!");
-			});
-			
-		});
-		
+
+            res.on('close', function (){
+                // Cuando se corta la conexión;
+                objeto_descarga.estado = 'error';
+                fs.unlink(ruta_descargas + archivo.name);
+            });
+        }).on('error', function(){
+            // Cuando se cae el servidor antes de iniciar el request;
+            objeto_descarga.estado = 'error';
+            fs.unlink(ruta_descargas + archivo.name);
+        });
+
 	}
-	
+
 	$scope.abrir = function(archivo) {
 		var ruta_con_dominio = archivo.url.replace('http://', '');
 		$location.path('/archivos/' + ruta_con_dominio);
 		$scope.filtro = '';
 	}
-	
+
 	//$scope.$watch('directorio', function() {
 	//	$scope.esta_en_directorio_raiz = /\/ls$/.test($scope.directorio);
 	//});
-	
+
 	$scope.regresar = function() {
 		history.back();
 	}
-	
+
 	var gui = require('nw.gui');
 	var ruta_compartidos = process.env.HOME + '/compartido/';
-	
+
 	$scope.abrir_carpeta_compartida = function() {
 		gui.Shell.openItem(ruta_compartidos);
 	}
-	
+
 	actualizar_listado();
 });
