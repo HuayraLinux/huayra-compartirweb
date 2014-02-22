4d1f7a253ce71e667dc36f4422c03865701a7625
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-02-22 02:47:19 -0300

    Indentando el archivo de preferencias.

--- src/assets/js/controllers/preferenciasCtrl.js
+++ src/assets/js/controllers/preferenciasCtrl.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 var http = require('http');
 var domain = require('domain');
@@ -9,49 +8,48 @@ var ruta_avatar = process.env.HOME + '/.huayra-compartir_avatar';
 app.controller("PreferenciasCtrl", function($scope, $http) {
     var preferencias = new Object();
     $scope.url_avatar = $scope.base + '/avatar';
-    
+
     $scope.cambiar_imagen_de_perfil = function() {
         var el = document.getElementById('fileDialog');
-        
+
         el.addEventListener("change", function(evt) {
-					var path_seleccionado = el.value;
-        	var comando = "convert '" + path_seleccionado + "' -thumbnail '100x100^' -gravity center -extent 100x100 png:" + ruta_avatar;
-            console.log(comando);
-					
-					function cuando_termina_conversion() {
-						var imagen_avatar = document.getElementById('imagen_avatar');
-						imagen_avatar.src = ruta_avatar + '?' + new Date()
-					}
-            
-          exec(comando, function(error, stdout, stderror) {
-              console.log({error: error,
-                           stdout: stdout,
-                           stderr: stderror});
-              
-              cuando_termina_conversion();
-          });
-          
-    		}, false);
-        
+            var path_seleccionado = el.value;
+            var comando = "convert '" + path_seleccionado + "' -thumbnail '100x100^' -gravity center -extent 100x100 png:" + ruta_avatar;
+
+            function cuando_termina_conversion() {
+                var imagen_avatar = document.getElementById('imagen_avatar');
+                imagen_avatar.src = ruta_avatar + '?' + new Date()
+            }
+
+            exec(comando, function(error, stdout, stderror) {
+                console.log({error: error,
+                             stdout: stdout,
+                             stderr: stderror});
+
+                cuando_termina_conversion();
+            });
+
+        }, false);
+
         el.click();
     }
 
     $http.get(ruta_preferencias).
         success(function (data, status){
-          preferencias = data
-          $scope.nombre = preferencias.nombre;
-        	$scope.frase = preferencias.frase;
+            preferencias = data
+            $scope.nombre = preferencias.nombre;
+            $scope.frase = preferencias.frase;
         }).
         error(function (){
-          preferencias.nombre = $scope.$parent.nombre;
-          preferencias.frase = $scope.$parent.frase;
+            preferencias.nombre = $scope.$parent.nombre;
+            preferencias.frase = $scope.$parent.frase;
         });
 
-		$scope.guardar_datos = function() {
-      preferencias.nombre = $scope.nombre;
-      preferencias.frase = $scope.frase;
-      window.guardar_preferencias(preferencias);
-			$scope.$parent.nombre = preferencias.nombre;
-			$scope.$parent.frase = preferencias.frase;
-		}
+    $scope.guardar_datos = function() {
+        preferencias.nombre = $scope.nombre;
+        preferencias.frase = $scope.frase;
+        window.guardar_preferencias(preferencias);
+        $scope.$parent.nombre = preferencias.nombre;
+        $scope.$parent.frase = preferencias.frase;
+    }
 });
