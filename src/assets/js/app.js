b91096d28eb2205a9c588abe04bc5784527d5ef9
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-02-22 00:04:48 -0300

    Normalizando tabs/espacios.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,11 @@
 /* Botón en la barra superior para recargar toda la aplicación. */
 window.actualizar = function() {
-	document.location.reload();
+    document.location.reload();
 }
 
 /* Botón en la barra superior para mostrar las herramientas de desarrollo. */
 window.mostrar_herramientas_de_desarrollo = function() {
-	gui.Window.get().showDevTools();
+    gui.Window.get().showDevTools();
 }
 
 window.guardar_preferencias = function(preferencias) {
@@ -26,42 +26,41 @@ var events = require('events');
 
 var app = angular.module('app', ['ngRoute', 'ngAnimate']);
 
-
 app.directive("progreso", function() {
 
-	function link(scope, element, attrs) {
-		scope.$watch(function() {
-			var porcentaje = scope.model.transmitido / scope.model.size * 100;
-			element.children(0).css('width', Math.floor(porcentaje) + "%");
-		});
-	}
+    function link(scope, element, attrs) {
+        scope.$watch(function() {
+            var porcentaje = scope.model.transmitido / scope.model.size * 100;
+            element.children(0).css('width', Math.floor(porcentaje) + "%");
+        });
+    }
 
-	return {
-		restrict: 'E',
-		replace: true,
-		template: '<div>' +
-							'<div style="background-color: green; height: 2px;"></div>' +
-							'{{model.transmitido / model.size * 100 | number:0}} %' +
-						  '</div>',
-		scope: {
-			model: '=',
-			porcentaje: '='
-		},
-		link: link
-	}
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div>' +
+                  '<div style="background-color: green; height: 2px;"></div>' +
+                  '{{model.transmitido / model.size * 100 | number:0}} %' +
+                  '</div>',
+        scope: {
+            model: '=',
+            porcentaje: '='
+        },
+        link: link
+    }
 });
 
 app.factory("Descargas", function() {
-	var descargas = [
-	];
+    var descargas = [
+    ];
 
-	return descargas;
+    return descargas;
 });
 
 
 app.factory('Eventos', function() {
   console.log("Creando el emisor de eventos!");
-	var eventos = new events.EventEmitter();
+    var eventos = new events.EventEmitter();
   return eventos;
 });
 
@@ -94,38 +93,38 @@ app.config(['$routeProvider', function($routeProvider) { $routeProvider.
             controller: 'ArchivosCtrl',
             templateUrl: 'partials/archivos.html'
           }).
-					otherwise({redirectTo:'/principal'});
+                    otherwise({redirectTo:'/principal'});
 }]);
 
 
 app.filter('bytes', function() {
-	return function(bytes, precision) {
-		if (bytes==0) return '...';
-		if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-		if (typeof precision === 'undefined') precision = 1;
-		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-			number = Math.floor(Math.log(bytes) / Math.log(1024));
-		return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
-	}
+    return function(bytes, precision) {
+        if (bytes==0) return '...';
+        if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+        if (typeof precision === 'undefined') precision = 1;
+        var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+            number = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+    }
 });
 
 app.controller("MainCtrl", function($scope, $location, $http, Descargas, Eventos) {
     var ruta_preferencias = process.env.HOME + '/.huayra-compartir';
     var data_preferencias = {};
     $scope.notificaciones = [];
-    
+
     $scope.getClass = function(path) {
-    	if ($location.path().substr(0, path.length) == path)
+        if ($location.path().substr(0, path.length) == path)
           return "active";
       else
           return "";
-  	}       
-    
+      }
+
     Eventos.on('inicia', function(data) {
           $scope.notificaciones.push(data);
           $scope.$apply();
     });
-        
+
     Eventos.on('finaliza', function(data) {
           for (var index in $scope.notificaciones) {
             if ($scope.notificaciones[index].id === data.id) {
@@ -136,7 +135,7 @@ app.controller("MainCtrl", function($scope, $location, $http, Descargas, Eventos
              }
            }
     });
-        
+
 
     function agregar_amigo(servicio) {
         for (var key in $scope.amigos) {
@@ -179,10 +178,10 @@ app.controller("MainCtrl", function($scope, $location, $http, Descargas, Eventos
             //console.log({evento: "up", servicio: servicio});
 
             // si el servicio es "huayra-compartir" copiamos el dict a nuestra lista de amigos
-          	if (servicio.name === "huayra-compartir") {
+              if (servicio.name === "huayra-compartir") {
 
-            	if (servicio.ip === servidor.mi_ip)
-             	  return; // Evita mostrar en la vista de amigos mi propio equipo.
+                if (servicio.ip === servidor.mi_ip)
+                   return; // Evita mostrar en la vista de amigos mi propio equipo.
 
 
               var tmp_url = "http://" + servicio.ip + ":" + servicio.port;
@@ -195,15 +194,15 @@ app.controller("MainCtrl", function($scope, $location, $http, Descargas, Eventos
               agregar_amigo(servicio);
 
 
-            	$scope.$apply();
+                $scope.$apply();
             }
           }
 
           function cuando_se_desconecta_un_equipo(nombre, servicio) {
             //console.log({evento: "down", servicio: servicio});
 
-        		for (var key in $scope.amigos) {
-             	if (servicio.id === $scope.amigos[key].id)
+                for (var key in $scope.amigos) {
+                 if (servicio.id === $scope.amigos[key].id)
                  $scope.amigos.splice(key, 1);
              }
 
@@ -215,87 +214,87 @@ app.controller("MainCtrl", function($scope, $location, $http, Descargas, Eventos
           $scope.base = servidor.base;
           $scope.mi_ip = servidor.mi_ip;
 
-    		});
+            });
 
 });
 
 app.controller("PrincipalCtrl", function($scope) {
-	var gui = require('nw.gui');
-	var ruta_compartidos = process.env.HOME + '/Compartido/';
+    var gui = require('nw.gui');
+    var ruta_compartidos = process.env.HOME + '/Compartido/';
 
-	$scope.abrir_carpeta_compartida = function() {
-		gui.Shell.openItem(ruta_compartidos);
-	}
+    $scope.abrir_carpeta_compartida = function() {
+        gui.Shell.openItem(ruta_compartidos);
+    }
 });
 
 app.controller("DescargasCtrl", function($scope, Descargas, $timeout) {
-	var gui = require('nw.gui');
-	var ruta_descargas = process.env.HOME + '/Descargas/';
+    var gui = require('nw.gui');
+    var ruta_descargas = process.env.HOME + '/Descargas/';
 
-	$scope.$parent.descargas_sin_ver = 0;
+    $scope.$parent.descargas_sin_ver = 0;
 
-	$scope.Descargas = Descargas;
-	var timer = null;
+    $scope.Descargas = Descargas;
+    var timer = null;
 
-	function actualizar_listado() {
-		timer = $timeout(actualizar_listado, 1000);
-	}
+    function actualizar_listado() {
+        timer = $timeout(actualizar_listado, 1000);
+    }
 
-	 $scope.abrir_directorio = function() {
-		gui.Shell.openItem(ruta_descargas);
-	}
+     $scope.abrir_directorio = function() {
+        gui.Shell.openItem(ruta_descargas);
+    }
 
-	 $scope.abrir_item = function(item) {
-		 gui.Shell.openItem(ruta_descargas + item.name);
-	 }
+     $scope.abrir_item = function(item) {
+         gui.Shell.openItem(ruta_descargas + item.name);
+     }
 
 
-	 $scope.limpiar_completados = function() {
-		 var lista_limpia = [];
+     $scope.limpiar_completados = function() {
+         var lista_limpia = [];
 
-		 for (var i=0; i<$scope.Descargas.length; i++) {
-			 if (Descargas[i].bajando)
-				 lista_limpia.push(Descargas[i]);
-		 }
+         for (var i=0; i<$scope.Descargas.length; i++) {
+             if (Descargas[i].bajando)
+                 lista_limpia.push(Descargas[i]);
+         }
 
-		 while ($scope.Descargas.length > 0) {
-    	Descargas.pop();
-		 }
+         while ($scope.Descargas.length > 0) {
+        Descargas.pop();
+         }
 
-		 for (var i=0; i<lista_limpia.length; i++) {
-			 $scope.Descargas.push(lista_limpia[i]);
-		 }
-	 }
+         for (var i=0; i<lista_limpia.length; i++) {
+             $scope.Descargas.push(lista_limpia[i]);
+         }
+     }
 
-	console.log("iniciando timer para actualizar progreso de las descargas.");
-	actualizar_listado();
+    console.log("iniciando timer para actualizar progreso de las descargas.");
+    actualizar_listado();
 
-	$scope.$on("$destroy", function(event) {
-		$timeout.cancel(timer);
-		console.log("cancelando el timer de actualización");
-	});
+    $scope.$on("$destroy", function(event) {
+        $timeout.cancel(timer);
+        console.log("cancelando el timer de actualización");
+    });
 
 });
 
 app.controller("NotificacionesCtrl", function($scope, Eventos) {
   $scope.notificaciones = $scope.$parent.notificaciones;
-    
+
   $scope.$parent.$watch('notificaciones', function() {
       console.log("Cambiaron las notificaciones");
   });
-    
-	
-	$scope.limpiar = function() {
-		$scope.$parent.notificaciones = [];
-		$scope.notificaciones = [];
-	}
+
+
+    $scope.limpiar = function() {
+        $scope.$parent.notificaciones = [];
+        $scope.notificaciones = [];
+    }
 });
 
 app.controller("AmigosCtrl", function($scope, $location) {
-	$scope.amigos = $scope.$parent.amigos;
+    $scope.amigos = $scope.$parent.amigos;
 
-	$scope.abrir_descargas_de = function(amigo) {
-		$location.path('/archivos/' + amigo.ip + ':' + amigo.port + '/obtener/');
-	}
+    $scope.abrir_descargas_de = function(amigo) {
+        $location.path('/archivos/' + amigo.ip + ':' + amigo.port + '/obtener/');
+    }
 });
 
