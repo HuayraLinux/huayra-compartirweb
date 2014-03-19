2efb9d200d4606fd6a572a09b6d7eeba6e078adc
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-03-18 22:42:13 -0300

    Arreglos de indexación.

--- src/assets/js/controllers/archivosCtrl.js
+++ src/assets/js/controllers/archivosCtrl.js
@@ -2,71 +2,70 @@ var fs = require('fs');
 var http = require('http');
 
 app.controller("ArchivosCtrl", function($scope, $http, $routeParams, $location, Descargas) {
-	$scope.esta_en_directorio_raiz = true;
-	$scope.archivos = [];
-	$scope.Descargas = Descargas;
-	$scope.filtro = '';
-	$scope.mi_ip = $scope.$parent.mi_ip;
-	$scope.es_vista_mis_archivos = true;
+    $scope.esta_en_directorio_raiz = true;
+    $scope.archivos = [];
+    $scope.Descargas = Descargas;
+    $scope.filtro = '';
+    $scope.mi_ip = $scope.$parent.mi_ip;
+    $scope.es_vista_mis_archivos = true;
 
-	var path = "";
+    var path = "";
 
-	var ruta_descargas = process.env.HOME + '/Descargas/';
+    var ruta_descargas = process.env.HOME + '/Descargas/';
 
-	// Genera el directorio descargas si no existe.
-	if (! fs.existsSync(ruta_descargas))
-		fs.mkdir(ruta_descargas);
+    // Genera el directorio descargas si no existe.
+    if (! fs.existsSync(ruta_descargas))
+        fs.mkdir(ruta_descargas);
 
-	if ($routeParams.url === undefined) {
-		var base_path = $scope.$parent.base;
-		var relative_path = "obtener/";
-		var path = base_path + '/' +  relative_path;
-	}
-	else {
-		var relative_path = $routeParams.url;
-		var path = 'http://' +  relative_path;
-	}
+    if ($routeParams.url === undefined) {
+        var base_path = $scope.$parent.base;
+        var relative_path = "obtener/";
+        var path = base_path + '/' +  relative_path;
+    }
+    else {
+        var relative_path = $routeParams.url;
+        var path = 'http://' +  relative_path;
+    }
 
-	function actualizar_listado() {
-		if ($routeParams.url) {
-			var host_a_visitar = $routeParams.url.split(':')[0];
-			console.log(host_a_visitar);
-			$scope.es_vista_mis_archivos = (host_a_visitar === $scope.mi_ip);
-		}
+    function actualizar_listado() {
+        if ($routeParams.url) {
+            var host_a_visitar = $routeParams.url.split(':')[0];
+            $scope.es_vista_mis_archivos = (host_a_visitar === $scope.mi_ip);
+        }
 
-    $scope.en_curso = true;
+        $scope.en_curso = true;
 
-    setTimeout(function() {
-        $http.get(path).success(function(data) {
-          $scope.en_curso = false;
-          $scope.archivos = data.archivos;
-        });
-    }, 500);
-	}
+        setTimeout(function() {
+            $http.get(path).success(function(data) {
+                $scope.en_curso = false;
+                $scope.archivos = data.archivos;
+            });
+        }, 500);
+    }
 
-	$scope.descargar = function(archivo) {
-		var objeto_descarga = archivo;
-		objeto_descarga.transmitido = 0;
-		$scope.Descargas.push(objeto_descarga);
-		$scope.$parent.descargas_sin_ver += 1;
+    $scope.descargar = function(archivo) {
+        var objeto_descarga = archivo;
+        objeto_descarga.transmitido = 0;
+        $scope.Descargas.push(objeto_descarga);
+        $scope.$parent.descargas_sin_ver += 1;
 
-		archivo.bajando = true;
+        archivo.bajando = true;
 
-		var file = fs.createWriteStream(ruta_descargas + archivo.name);
+        var file = fs.createWriteStream(ruta_descargas + archivo.name);
 
-		http.get(archivo.url, function(res) {
+        http.get(archivo.url, function(res) {
 
-			res.on('data', function(chunk) {
-				file.write(chunk);
-				objeto_descarga.transmitido += chunk.length;
-			});
+            res.on('data', function(chunk) {
+                file.write(chunk);
+                objeto_descarga.transmitido += chunk.length;
+            });
 
-			res.on('end', function() {
-				objeto_descarga.transmitido = objeto_descarga.size;
-				objeto_descarga.bajando = false;
+            res.on('end', function() {
+                objeto_descarga.transmitido = objeto_descarga.size;
+                objeto_descarga.bajando = false;
                 objeto_descarga.estado = 'success';
-				//file.close(); // produce un bug porque cierra el archivo mientras que está copiando. El archivo queda truncado.
-			});
+                //file.close(); // produce un bug porque cierra el archivo mientras que está copiando. El archivo queda truncado.
+            });
 
             res.on('close', function (){
                 // Cuando se corta la conexión;
@@ -79,28 +78,28 @@ app.controller("ArchivosCtrl", function($scope, $http, $routeParams, $location, 
             fs.unlink(ruta_descargas + archivo.name);
         });
 
-	}
+    }
 
-	$scope.abrir = function(archivo) {
-		var ruta_con_dominio = archivo.url.replace('http://', '');
-		$location.path('/archivos/' + ruta_con_dominio);
-		$scope.filtro = '';
-	}
+    $scope.abrir = function(archivo) {
+        var ruta_con_dominio = archivo.url.replace('http://', '');
+        $location.path('/archivos/' + ruta_con_dominio);
+        $scope.filtro = '';
+    }
 
-	//$scope.$watch('directorio', function() {
-	//	$scope.esta_en_directorio_raiz = /\/ls$/.test($scope.directorio);
-	//});
+    //$scope.$watch('directorio', function() {
+    //    $scope.esta_en_directorio_raiz = /\/ls$/.test($scope.directorio);
+    //});
 
-	$scope.regresar = function() {
-		history.back();
-	}
+    $scope.regresar = function() {
+        history.back();
+    }
 
-	var gui = require('nw.gui');
-	var ruta_compartidos = process.env.HOME + '/Compartido/';
+    var gui = require('nw.gui');
+    var ruta_compartidos = process.env.HOME + '/Compartido/';
 
-	$scope.abrir_carpeta_compartida = function() {
-		gui.Shell.openItem(ruta_compartidos);
-	}
+    $scope.abrir_carpeta_compartida = function() {
+        gui.Shell.openItem(ruta_compartidos);
+    }
 
-	actualizar_listado();
+    actualizar_listado();
 });
